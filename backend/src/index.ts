917132d8d3363bc5f176b8dd7208d73844638a07
panosdb <ppsimatikas@gmail.com>
2024-08-05 16:20:16 +0200

    Call contract from BE and UI

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -9,11 +9,47 @@
 
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
+import {ethers} from "ethers";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+const contractABI = [
+  "function message() public view returns (string)",
+  "function setMessage(string memory _message) public",
+];
 
-export const helloWorld = onRequest((request, response) => {
+function getProvider() {
+  return new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT);
+}
+
+async function getBalance(wallet: ethers.Wallet) {
+  return ethers.formatEther(await getProvider().getBalance(wallet.address));
+}
+
+async function setMessage(wallet: ethers.Wallet, newMessage: string) {
+  const contractWithSigner = new ethers.Contract(
+    contractAddress, contractABI, wallet
+  );
+
+  const tx = await contractWithSigner.setMessage(newMessage);
+  await tx.wait();
+
+  logger.info("New message set: " + newMessage, {structuredData: true});
+}
+
+export const helloWorld = onRequest(async (request, response) => {
+  if (!process.env.MASTER_WALLET_PK) {
+    response.status(500).send({
+      "error": "set the MASTER_WALLET_PK environment variable",
+    });
+    return;
+  }
+
+  const provider = getProvider();
+  const wallet = new ethers.Wallet(process.env.MASTER_WALLET_PK, provider);
+
   logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+  logger.info(await getBalance(wallet), {structuredData: true});
+  await setMessage(wallet, "Hello panos");
+  logger.info(await getBalance(wallet), {structuredData: true});
+  response.send(wallet.privateKey);
 });
--- ui/src/pages/Home.tsx
+++ ui/src/pages/Home.tsx
@@ -1,10 +1,26 @@
 import {Stack} from "@mantine/core";
+import {useEffect, useState} from "react";
+import {ethers} from "ethers";
 
 
 function Home() {
+    const [ message, setMessage ] = useState<string>('');
+
+    const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
+    const contractABI = [
+        "function message() public view returns (string)"
+    ];
+    const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545/");
+
+    useEffect(() => {
+        const contract = new ethers.Contract(contractAddress, contractABI, provider);
+        contract.message().then(setMessage);
+    }, []);
+
     return (
         <Stack>
             <h1>Vote for the 2024 U.S. elections</h1>
+            <b>{message}</b>
         </Stack>
     );
 }
