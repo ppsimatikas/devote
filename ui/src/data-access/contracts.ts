7fbf87d2055c0afe72210a55a2917545b7e330c1
panosdb <ppsimatikas@gmail.com>
2024-08-06 10:19:48 +0200

    add mock data

--- ui/src/data-access/contracts.ts
+++ ui/src/data-access/contracts.ts
@@ -1,6 +1,7 @@
 import {useQuery} from '@tanstack/react-query';
 import {getCandidates, getVotes} from "../services/contracts";
 import {candidates} from "../data/candidates";
+import states from "../data/states.json";
 
 export function useCandidates() {
     return useQuery({
@@ -15,22 +16,28 @@ export function useCandidates() {
     })
 }
 
-// function mockData() {
-//     return states.features.reduce((stateVotes, state: any) => {
-//         stateVotes[state.properties.state] = candidates.reduce((votes: any, party) => {
-//             votes[party.title] = Math.floor(Math.random() * 1000001)
-//             return votes;
-//         }, {})
-//         return stateVotes;
-//     }, {} as any)
-// }
+function mockData() {
+    return states.features.reduce((stateVotes, state: any) => {
+        const addState = Math.floor(Math.random() * 2);
+        stateVotes[state.properties.state] = candidates.reduce((votes: any, party) => {
+            votes[party.title] = addState ? Math.floor(Math.random() * 1000001) : 0;
+            return votes;
+        }, {})
+        return stateVotes;
+    }, {} as any)
+}
 
 function votesToData(votes: any[]) {
     return votes.reduce((stateVotes, state: any) => {
-        stateVotes[state.state] = state.votes.reduce((votes: any, party: any) => {
-            votes[party.candidate] = Number(party.votes);
-            return votes;
-        }, {} as any);
+        const hasData = state.votes.find((p: any) => Number(p.votes) > 0)
+
+        if (hasData) {
+            stateVotes[state.state] = state.votes.reduce((votes: any, party: any) => {
+                votes[party.candidate] = Number(party.votes);
+                return votes;
+            }, {} as any);
+        }
+
         return stateVotes;
     }, {} as any);
 }
@@ -40,7 +47,9 @@ export function useResults() {
         queryKey: ['results'],
         queryFn: async () => {
             const votes = await getVotes();
-            return votesToData(votes);
+            const onchainVotes = votesToData(votes);
+            const mocks = mockData();
+            return {...mocks, ...onchainVotes};
         },
         staleTime: 360, // 1 minute
     })
