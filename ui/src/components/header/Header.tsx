b30c77efa3d76b18be9b4ff77501dcc2bc64ca76
panosdb <ppsimatikas@gmail.com>
2024-08-11 09:04:34 +0300

    Add about

--- ui/src/components/header/Header.tsx
+++ ui/src/components/header/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {Anchor, Box, Button, Container, Group} from '@mantine/core';
+import {Anchor, Box, Button, Container, Group, Text} from '@mantine/core';
 import {Link, useLocation} from 'react-router-dom';
 import styles from './Header.module.css';
 import {gradient} from "../../theme";
 import {useUiBreakpoints} from "../../utils/use-ui-breakpoints";
+import {IconHelpOctagon} from "@tabler/icons-react";
 
 
 function Header() {
@@ -17,7 +18,7 @@ function Header() {
                 <Anchor component={Link} to="/" className={styles.logo}>
                     <img src="logo.png" alt="Logo" width={imageSize} height={imageSize}/>
                 </Anchor>
-                <Group justify={isSm ? "flex-end" : "center"}>
+                <Group justify={isSm ? "flex-end" : "center"} mr={40}>
                     <Button
                         component={Link}
                         to="/"
@@ -39,6 +40,15 @@ function Header() {
                         Results
                     </Button>
                 </Group>
+                <Anchor
+                    href='https://sites.google.com/view/about-devote/home' target="_blank"
+                    className={styles.about}
+                >
+                    <Group gap={5}>
+                        <Text size='20px' fw="bold" hidden={isSm}>About</Text>
+                        <IconHelpOctagon size={30}/>
+                    </Group>
+                </Anchor>
             </Container>
         </Box>
     );
